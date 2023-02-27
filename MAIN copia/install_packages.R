88891423ca08449c09b9d0e331138c89028b7fec
Camilla Pelagalli <50362663+Camilla9347@users.noreply.github.com>
2023-02-27 08:55:10 +0100

    Delete MAIN copia directory

--- MAIN copia/install_packages.R
+++
@@ -1,7 +0,0 @@
-list_of_packages <- c("readr", "ParallelPC","parallel","pcalg")
-
-new_packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
-
-if(length(new_packages)){
-  install.packages(new_packages)
-} 
